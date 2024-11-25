33b80dd97a2e15dfb62f4063990a4af78d6ddb55
Louise Ducrocq <louise.ducroc@gmail.com>
2024-11-25 09:56:23 +0100

    commit

--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -7,13 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Ajout du CORS
 
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-        builder.WithOrigins("http://localhost:3000")
-               .AllowAnyMethod()
-               .AllowAnyHeader());
-});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<APIContext>
--- MovieApi/obj/Debug/net8.0/MovieApi.AssemblyInfo.cs
+++ MovieApi/obj/Debug/net8.0/MovieApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd566b809b5186f4323ec59f68837defd2112d76")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
